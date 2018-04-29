d3768c1bce507493c12caffb509012fce4781c3f
Josh Hernandez <yatchualee@gmail.com>
2018-04-29 01:42:16 -0400

    Number Generator Thread
    Reading in command arguments and initiating the number generator thread.

--- src/JavaThreads.java
+++ src/JavaThreads.java
@@ -1,38 +1,57 @@
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
 
 import static java.lang.Integer.parseInt;
+
+class generatorThread extends Thread{
+    public void start(){
+    }
+
+    public int generateNumber(int interval){
+        Random rando = new Random();
+        int  n = rando.nextInt();
+
+        System.out.println("Thread " + Thread.currentThread().getId() + ": Value " + n);
+        try {
+            Thread.sleep(interval);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return n;
+    }
+}
 
 public class JavaThreads {
 
     public static void main(String[] args) {
 
-        Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> numberQueue = new LinkedList<>();
+        //Queue<Thread> threadQueue = new LinkedList<>();
 
-        // Adds elements {0, 1, 2, 3, 4} to queue
-        for (int i=0; i<args.length; i++){
-            //Convert command line arguments to integers
-            q.add(parseInt(args[i]));
-        }
+        // Adds elements to queue
+        for (int i=0; i<args.length; i++)
+            numberQueue.add(parseInt(args[i]));
+
+        generatorThread myThread = new generatorThread();
+        myThread.start();
+
+        for(int i=0; i<5; i++)
+            numberQueue.add(myThread.generateNumber(parseInt(args[0])));
 
         // Display contents of the queue.
-        System.out.println("Elements of queue: " + q);
+        System.out.println("Elements of queue: " + numberQueue);
         //System.out.println(q);
 
-        // To remove the head of queue.
-        int removedele = q.remove();
-        System.out.println("removed element: " + removedele);
+        //int removedele = q.remove();
+        //System.out.println("removed element: " + removedele);
 
         // To view the head of queue
-        int head = q.peek();
+        int head = numberQueue.peek();
         System.out.println("head of queue: " + head);
 
-        // Rest all methods of collection interface,
-        // Like size and contains can be used with this
-        // implementation.
-        int size = q.size();
+        int size = numberQueue.size();
         System.out.println("Size of queue: " + size);
 
     }
 }
-
